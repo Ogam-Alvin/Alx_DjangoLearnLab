0b311eb6fb9af9fc7e5d3a20224f4ca3648f0e19
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-15 12:28:39 +0300

    Task 4 done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views  # 🔴 Must import the module, not individual functions
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
@@ -9,6 +10,10 @@ urlpatterns = [
 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),  # 🔴 checker wants views.register
+    path('register/', views.register, name='register'), 
+
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
 ]
 
