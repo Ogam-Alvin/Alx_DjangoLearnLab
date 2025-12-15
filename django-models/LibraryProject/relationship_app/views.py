667bf200f2d6eb0e540be8cb2ca082468888e4aa
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-15 12:04:28 +0300

    Task 2 update 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
-# Create your views here.
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.shortcuts import render
+from .models import Book
 
-# Function-based view
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.select_related('author').all()
+    return render(
+        request,
+        'relationship_app/list_books.html',  # 🔴 MUST be this exact string
+        {'books': books}
+    )
