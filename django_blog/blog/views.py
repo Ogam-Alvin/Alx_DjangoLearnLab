1261e1b8f98c583a62ee9b905d53519931716654
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-19 16:24:43 +0300

    Task 1 done

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "blog/logout.html")
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email")
+        request.user.save()
+    return render(request, "blog/profile.html")
