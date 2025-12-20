d19860daac4498b4473828401c9967466bd157ea
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-20 10:25:01 +0300

    task 4 update 4

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
+from taggit.forms import TagWidget
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -11,10 +12,13 @@ class RegisterForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        help_text="Add tags separated by commas"
-    )
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+
+        widgets = {                   
+            'tags': TagWidget(),      
+        }
 
     class Meta:
         model = Post
