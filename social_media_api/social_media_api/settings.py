1bfcdd3de153cfde42c542eef0c2cb7eaaf5596f
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-22 09:01:48 +0300

    task 1 done

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,14 @@ REST_FRAMEWORK = {
     ],
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
